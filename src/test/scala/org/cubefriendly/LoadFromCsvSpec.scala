fa3cbe2a6d75a33ceda5eb9851ee39654f105dfd
David Roon <david@roon.me>
2015-05-19 07:49:56 +0200

    reformat code

--- src/test/scala/org/cubefriendly/LoadFromCsvSpec.scala
+++ src/test/scala/org/cubefriendly/LoadFromCsvSpec.scala
@@ -11,9 +11,9 @@ import org.specs2.mutable.Specification
  * Cubefriendly
  * Created by david on 03.03.15.
  */
-class LoadFromCsvSpec extends Specification{
+class LoadFromCsvSpec extends Specification {
   "A Cube data" should {
-    def db():DB = DBMaker.tempFileDB().make()
+    def db(): DB = DBMaker.tempFileDB().make()
 
     "be loaded from CSV" in {
       val cubeName = "test_cube"
@@ -24,7 +24,12 @@ class LoadFromCsvSpec extends Specification{
         db = mapdb,
         config = CubeConfig(
           name = cubeName,
-          metrics = Seq("IMD SCORE", "INCOME SCORE", "EMPLOYMENT SCORE", "HEALTH DEPRIVATION AND DISABILITY SCORE", "EDUCATION SKILLS AND TRAINING SCORE", "BARRIERS TO HOUSING AND SERVICES SCORE", "CRIME AND DISORDER SCORE", "LIVING ENVIRONMENT SCORE", "Indoors Sub-domain Score", "Outdoors Sub-domain Score", "Geographical Barriers Sub-domain Score", "Wider Barriers Sub-domain Score", "Children/Young People Sub-domain Score", "Skills Sub-domain Score")))
+          metrics = Seq("IMD SCORE", "INCOME SCORE", "EMPLOYMENT SCORE",
+            "HEALTH DEPRIVATION AND DISABILITY SCORE", "EDUCATION SKILLS AND TRAINING SCORE",
+            "BARRIERS TO HOUSING AND SERVICES SCORE", "CRIME AND DISORDER SCORE",
+            "LIVING ENVIRONMENT SCORE", "Indoors Sub-domain Score", "Outdoors Sub-domain Score",
+            "Geographical Barriers Sub-domain Score", "Wider Barriers Sub-domain Score",
+            "Children/Young People Sub-domain Score", "Skills Sub-domain Score")))
       actual.name must be equalTo cubeName
       mapdb.close()
       success
